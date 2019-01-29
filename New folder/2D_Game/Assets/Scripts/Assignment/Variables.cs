dc70a05817cb6e74fca5e72fbaeb90db907c3ee5
Mr_Giggles <banderson7370@gmail.com>
2019-01-28 17:17:58 -0700

    went over some variables basics

---
+++ New folder/2D_Game/Assets/Scripts/Assignment/Variables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+
+	 public static int number;
+	 public float speed;
+	 public string name;
+
+	// Use this for initialization
+        /*multi
+		line
+		comment
+		dood
+		 */
+	void Start () {
+		//?number = 10;
+		speed = 1.84f;
+		name = "Bob";
+
+		print(name + " was " +number+ " years old!!" + speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (number,0,0);
+		
+	}
+}
