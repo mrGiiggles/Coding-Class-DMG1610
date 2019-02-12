9f569524780090f114a539d4a12fa3c5724281b5
Mr_Giggles <banderson7370@gmail.com>
2019-02-11 17:17:55 -0700

    slight edit

--- New folder/2D_Game/Assets/Scripts/Assignment/Coin.cs
+++ New folder/2D_Game/Assets/Scripts/Assignment/Coin.cs
@@ -2,7 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour {
+public class Coin : GenericPickup {
+
+	public GameObject PC;
+
+	void onTriggerEnter2D(collider2D other){
+		if(GameObject.other.name == "PC") {
+			//player collect coin
+		}
+
+		else{
+			//not [layer]
+		}
+
+	}
 
 	// Use this for initialization
 	void Start () {
