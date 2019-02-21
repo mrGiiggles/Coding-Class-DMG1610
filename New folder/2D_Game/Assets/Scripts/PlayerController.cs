8f4d1482cdeabc5fc6b4d02bc7176b7f5b056a67
Mr_Giggles <banderson7370@gmail.com>
2019-02-20 17:12:56 -0700

    can collect coins

--- New folder/2D_Game/Assets/Scripts/PlayerController.cs
+++ New folder/2D_Game/Assets/Scripts/PlayerController.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+	//player movement variables
 	public float moveSpeed;
 	public float jumpHeight;
 
+	//player grounded variables
+	private bool grounded;
 
 
 	// Use this for initialization
 	void Start () {
+		// default to true on grounded
+		grounded = true;
 		
 	}
+
+	//this and && that
+	//this or || that
+	// !Not
 	
 	// Update is called once per frame
 	void Update () {
+		//moves player left & right
 		if(Input.GetKey(KeyCode.D)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
@@ -23,7 +32,10 @@ public class PlayerController : MonoBehaviour {
 		else if(Input.GetKey(KeyCode.A)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
-		else if(Input.GetKey(KeyCode.W)){
+			//Makes player jump
+		
+		
+		 if(Input.GetKeyDown(KeyCode.W) && grounded){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
 		}
 	}
