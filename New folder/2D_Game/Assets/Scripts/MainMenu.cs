e45d1d403a83da5f61eec277b43539e18a092ff0
Mr_Giggles <banderson7370@gmail.com>
2019-03-13 17:13:09 -0600

    main menu created

---
+++ New folder/2D_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+//index level to load
+public int levelToLoad;
+//this function loads the level stored in the levelToLoad variable after button is pressed
+public void LoadLevel() {
+	SceneManager.LoadScene(levelToLoad);
+}
+//this function quits the game after the button is pressed
+ public void LevelExit() {
+	 Application.Quit();
+ }
+}
