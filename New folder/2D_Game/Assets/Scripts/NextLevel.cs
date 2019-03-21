8d750f5777dd9d7cd50cb43f70e01eb48ae2a62e
Unknown <banderson7370@gmail.com>
2019-03-20 20:42:59 -0600

    after much trial, i can now move between levels

---
+++ New folder/2D_Game/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+	
+	public string level;
+
+
+	private void OnTriggerEnter2D(Collider2D col) {
+		switch(col.tag) {
+			case "Player":
+			SceneManager.LoadScene(level);
+			break;
+		}
+		
+			print("This should be working");
+		
+	
+}
+}
