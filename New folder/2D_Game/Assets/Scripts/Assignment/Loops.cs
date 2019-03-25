092ae70e067321274ac42108f2df7ad38b157c06
Mr_Giggles <banderson7370@gmail.com>
2019-03-25 17:14:28 -0600

    enemy simple movement

---
+++ New folder/2D_Game/Assets/Scripts/Assignment/Loops.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	private int bottles= 1;
+	public bool isRaining = true;
+
+	// Use this for initialization
+	void Start () {
+		// //for loop
+		// for(int i=100; i >= bottles; i++){
+		// 	print(i + " bottles of ginger-beer on the wall");
+		// }
+
+		while(bottles <= 100){
+			//print("It's raining, it's pouring, the old man is snoring!");
+			print(bottles + " bottles of ginger-beer on the wall");
+			bottles ++;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New folder/2D_Game/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+
+	//movement variables
+	public float moveSpeed;
+	public bool moveRight;
+
+	//wall check
+	public Transform wallCheck;
+	public float wallCheckRadius;
+	public LayerMask whatIsWall;
+	private bool hittingWall;
+
+	//Edge Check
+	private bool notAtEdge;
+	public Transform edgeCheck;
+
+	
+	// Update is called once per frame
+	void Update () {
+		notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+		hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+		if (hittingWall || !notAtEdge) {
+			moveRight = !moveRight;
+		}
+		if (moveRight) {
+			transform.localScale = new Vector3(-1f, 1f, 1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+
+		else {
+			transform.localScale = new Vector3(1f, 1f ,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
