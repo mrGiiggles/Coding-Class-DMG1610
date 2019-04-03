5fb5e5a656181ff45bc35631d87a20c9919cedad
Mr_Giggles <banderson7370@gmail.com>
2019-04-03 17:14:16 -0600

    pushing this up

---
+++ New folder/2D_Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public PlayerController player;
+
+	public bool isFollowing;
+
+	//Camera Poisition offset
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController>();
+
+		isFollowing = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFollowing) {
+			transform.position = new Vector3( player.transform.position.x + xOffset, player.transform.position.y + yOffset, transform.position.z);
+		}
+		
+	}
+}
---
+++ New folder/2D_Game/Assets/Scripts/DestroyParticles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticles : MonoBehaviour {
+
+	private ParticleSystem thisParticleSystem;
+
+
+	// Use this for initialization
+	void Start () {
+		thisParticleSystem = GetComponent<ParticleSystem>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (thisParticleSystem.isPlaying)
+			return;
+
+			Destroy (gameObject);
+	}
+
+	void onBecomeInvisible () {
+		Destroy(gameObject);
+	}
+}
