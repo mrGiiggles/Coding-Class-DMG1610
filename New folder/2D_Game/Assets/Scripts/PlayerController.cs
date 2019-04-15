34266090343b1a550f6ef9f43cd11bab4a58771b
Mr_Giggles <banderson7370@gmail.com>
2019-04-15 17:15:39 -0600

    setup for animation

--- New folder/2D_Game/Assets/Scripts/PlayerController.cs
+++ New folder/2D_Game/Assets/Scripts/PlayerController.cs
@@ -13,10 +13,15 @@ public class PlayerController : MonoBehaviour {
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
 
+	// animation variable
+	public Animator animator;
+
 
 	// Use this for initialization
 	void Start () {
 		// default to true on grounded
+		animator.SetBool("isWalking", false);
+		animator.SetBool("isJumping", false);
 		
 	}
 
@@ -33,15 +38,30 @@ public class PlayerController : MonoBehaviour {
 		//moves player left & right
 		if(Input.GetKey(KeyCode.D)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			//moveVelocity = moveSpeed;
+			animator.SetBool("isWalking", true);
 		}
-
-		else if(Input.GetKey(KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			//jump released
+		else if (Input.GetKeyUp(KeyCode.D)){
+			animator.SetBool("isWalking", false);
 		}
 			
+		else if(Input.GetKey(KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			//moveVelocity = moveSpeed
+			animator.SetBool("isWalking", true);
+		}
+		else if (Input.GetKeyUp(KeyCode.A)){
+			animator.SetBool("isWalking", false);
+		}
+
 			//Makes player jump
 		if(Input.GetKeyDown(KeyCode.W) && grounded){
 			Jump();
+		}
+
+		if (grounded) {
+			animator.SetBool("isJumping", false);
 		}
 
 		//player flip
@@ -55,5 +75,6 @@ public class PlayerController : MonoBehaviour {
 
 	void Jump() {
 		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		animator.SetBool("isJumping", true);
 	}
 }
--- New folder/2D_Game/Assets/Scripts/Teleport.cs
+++ New folder/2D_Game/Assets/Scripts/Teleport.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Teleport : MonoBehaviour {
+// public class Teleport : MonoBehaviour {
 
-public GameObject gate;
-public GameObject player;
+// public GameObject gate;
+// public GameObject player;
 
 
-private void OnTriggerEnter2D(Collider2D col){
-	if (col.gameobject.tag == "Player") {
-		StartCoroutine (Yeet());
-	}
-}
+// private void OnTriggerEnter2D(Collider2D col){
+// 	if (col.gameobject.tag == "Player") {
+// 		StartCoroutine (Yeet());
+// 	}
+// }
 
-IEnumerator Yeet() {
-	yield return new WaitForSeconds(1);
-	}
+// IEnumerator Yeet() {
+// 	yield return new WaitForSeconds(1);
+// 	}
 	
-}
+// }
